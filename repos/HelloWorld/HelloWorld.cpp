aa2b2a20714b702efd0834524f67fb67767160f3
FyreTitan <KentonYoung@users.noreply.github.com>
2021-01-31 18:03:11 -0700

    Additional C++ Learning Code

---
+++ repos/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// A C++ Program to output a greeting.
+
+int main()
+{
+	cout << "Hello World!" << endl;
+
+	int firstNumber;
+	firstNumber = 12;
+	cout << "firstNumber = " << firstNumber << endl;
+
+	float firstFloat = 0.12345;
+	cout << "firstFloat = " << firstFloat << endl;
+
+	double firstDouble; firstDouble = 0.123456789;
+	cout << "firstDouble = " << firstDouble << endl;
+
+	bool isMyFirstBool = false;
+	cout << "isMyFirstBool = " << isMyFirstBool << endl;
+
+	char firstChar; firstChar='A';
+	cout << "firstChar = " << firstChar << endl;
+
+	return 0 ;
+}
---
+++ repos/HelloWorld/VariableDeclarations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Practicing delcaring then/and initializing variables
+
+int main()
+{
+	int firstNumber; 
+	firstNumber = 12;
+	cout << firstNumber << endl;
+	return 0;
+}
